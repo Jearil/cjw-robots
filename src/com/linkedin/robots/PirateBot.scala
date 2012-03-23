6a8078ce1f488e3417b321c8d666f4ee299d36eb
Colin Miller <jearil@gmail.com>
2012-03-23 11:04:11 -0700

    Empty PirateBot (because I like pirates)

---
+++ src/com/linkedin/robots/PirateBot.scala
@@ -0,0 +1,17 @@
+package com.linkedin.robots
+
+import robocode.robotinterfaces.peer.IBasicRobotPeer
+import java.io.PrintStream
+import robocode.Robot
+import robocode.ScannedRobotEvent
+
+class PirateBot extends Robot {
+
+  override def run() {
+    
+  }
+  
+  override def onScannedRobot(event : ScannedRobotEvent) {
+    
+  }
+}
