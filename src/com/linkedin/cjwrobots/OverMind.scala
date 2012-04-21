2207ce180f551af81bc8b219843b298b6cf94ae9
Walter Fender <wfender@wfendner-mn.local>
2012-04-21 13:37:28 -0700

    Local change

--- src/com/linkedin/cjwrobots/OverMind.scala
+++ src/com/linkedin/cjwrobots/OverMind.scala
@@ -1,11 +1,28 @@
 package com.linkedin.cjwrobots
 
+import robocode.ScannedRobotEvent
+import java.lang.Double
+
 /**
  * Singleton that controls all of the other robots.
  */
 object OverMind {
+  var enemyRange : Double = null
   
   def register(bot : Hydralisk) {
     
+  }
+
+  def onScannedRobot(bot : Hydralisk, event : ScannedRobotEvent) {
+    if (isTeammate(e.getName)) {
+      return
+    }
+    var heading: Double = e.getHeading
+    var bearing: Double = e.getBearing
+    var range: Double = e.getDistance
+    if (enemyRange == null || enemyRange.doubleValue > range) {
+      enemyRange = range
+      enemyBearing = normalizeHeading(heading + bearing)
+    }
   }
 }
