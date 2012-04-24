da88dd993db21a1de17321302975e7cf42659003
Walter Fender <wfender@wfendner-mn.gateway.2wire.net>
2012-04-23 22:48:54 -0700

    Saving before pulling in Jacobs changes. Made simple chane to convert to team robot.

--- src/main/java/com/linkedin/cjwrobot/RumbleBot.java
+++ src/main/java/com/linkedin/cjwrobot/RumbleBot.java
@@ -15,103 +15,106 @@ import robocode.util.Utils;
 
 //$Id$
 
-public abstract class RumbleBot extends AdvancedRobot {
-	boolean isTC = false; // http://robowiki.net/?TargetingChallenge
-	boolean isMC = false; // http://robowiki.net/?MovementChallenge
-	boolean doGL = false; // http://robowiki.net/?CassiusClay/GL
+public abstract class RumbleBot extends TeamRobot {
+    boolean isTC = false; // http://robowiki.net/?TargetingChallenge
+    boolean isMC = false; // http://robowiki.net/?MovementChallenge
+    boolean doGL = false; // http://robowiki.net/?CassiusClay/GL
 
-	static double wins;
-	static int skipped;
+    static double wins;
+    static int skipped;
 
-	Butterfly floater;
-	Stinger stinger;
-	RobotPredictor robotPredictor = new RobotPredictor();
-	int timeSinceScan = 0;
-	ScannedRobotEvent lastScanEvent;
-	static long scans;
-	static double enemyApproachVelocity;
+    Butterfly floater;
+    Stinger stinger;
+    RobotPredictor robotPredictor = new RobotPredictor();
+    int timeSinceScan = 0;
+    ScannedRobotEvent lastScanEvent;
+    static long scans;
+    static double enemyApproachVelocity;
 
 
-	public void run() {
-		Stinger.isTC = isTC;
-		Butterfly.isMC = isMC;
-		Butterfly.doGL = doGL;
-		setAdjustRadarForGunTurn(true);
-		setAdjustGunForRobotTurn(true);
+    public void run() {
+        Stinger.isTC = isTC;
+        Butterfly.isMC = isMC;
+        Butterfly.doGL = doGL;
+        setAdjustRadarForGunTurn(true);
+        setAdjustGunForRobotTurn(true);
 
-		System.out.println("Skipped turns: " + skipped);
-		do {
-			if (timeSinceScan++ > 1) {
-				setTurnRadarRightRadians(Double.POSITIVE_INFINITY); 
-			}
-			if (getOthers() == 0) {
-				onScannedRobot(lastScanEvent);
-			}
-			execute();
-		} while (true);
-	}
+        System.out.println("Skipped turns: " + skipped);
+        do {
+            if (timeSinceScan++ > 1) {
+                setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
+            }
+            if (getOthers() == 0) {
+                onScannedRobot(lastScanEvent);
+            }
+            execute();
+        } while (true);
+    }
 
-	public void onScannedRobot(ScannedRobotEvent e) {
-		scans++;
-		double enemyAbsBearing = getHeadingRadians() + e.getBearingRadians();
-		if (getOthers() > 0 && scans > 0) {
-			enemyApproachVelocity = PUtils.rollingAvg(enemyApproachVelocity, e.getVelocity() * -Math.cos(e.getHeadingRadians() - enemyAbsBearing), Math.min(scans, 5000));
-		}
+    public void onScannedRobot(ScannedRobotEvent e)
+    {
 
-		lastScanEvent = e;
-		timeSinceScan = 0;
-		setTurnRadarRightRadians(Utils.normalRelativeAngle(enemyAbsBearing - getRadarHeadingRadians()) * 2);
-		if (!isTC) {
-			floater.onScannedRobot(e);
-		}
-		if (!isMC) {
-			stinger.onScannedRobot(e);
-		}
-	}
 
-	public void onHitByBullet(HitByBulletEvent e) {
-		floater.onHitByBullet(e);
-	}
+        scans++;
+        double enemyAbsBearing = getHeadingRadians() + e.getBearingRadians();
+        if (getOthers() > 0 && scans > 0) {
+            enemyApproachVelocity = PUtils.rollingAvg(enemyApproachVelocity, e.getVelocity() * -Math.cos(e.getHeadingRadians() - enemyAbsBearing), Math.min(scans, 5000));
+        }
 
-	public void onBulletHit(BulletHitEvent e) {
-		floater.onBulletHit(e);
-		stinger.onBulletHit(e);
-	}
+        lastScanEvent = e;
+        timeSinceScan = 0;
+        setTurnRadarRightRadians(Utils.normalRelativeAngle(enemyAbsBearing - getRadarHeadingRadians()) * 2);
+        if (!isTC) {
+            floater.onScannedRobot(e);
+        }
+        if (!isMC) {
+            stinger.onScannedRobot(e);
+        }
+    }
 
-	public void onBulletHitBullet(BulletHitBulletEvent e) {
-		floater.onBulletHitBullet(e);
-	}
+    public void onHitByBullet(HitByBulletEvent e) {
+        floater.onHitByBullet(e);
+    }
 
-	public void onWin(WinEvent e) {
-		stinger.roundOver();
-		wins++;
-	}
+    public void onBulletHit(BulletHitEvent e) {
+        floater.onBulletHit(e);
+        stinger.onBulletHit(e);
+    }
 
-	public void onDeath(DeathEvent e) {
-		stinger.roundOver();
-	}
+    public void onBulletHitBullet(BulletHitBulletEvent e) {
+        floater.onBulletHitBullet(e);
+    }
 
-	public void onSkippedTurn(SkippedTurnEvent e) {
-		System.out.println("skipped turn! time = " + getTime());
-		skipped++;
-	}
+    public void onWin(WinEvent e) {
+        stinger.roundOver();
+        wins++;
+    }
 
-	public void setTurnRightRadians(double turn) {
-		super.setTurnRightRadians(turn);
-		robotPredictor.setTurnRightRadians(turn);
-	}
+    public void onDeath(DeathEvent e) {
+        stinger.roundOver();
+    }
 
-	public void setAhead(double d) {
-		super.setAhead(d);
-		robotPredictor.setAhead(d);
-	}
+    public void onSkippedTurn(SkippedTurnEvent e) {
+        System.out.println("skipped turn! time = " + getTime());
+        skipped++;
+    }
 
-	public void setMaxVelocity(double v) {
-		super.setMaxVelocity(v);
-		robotPredictor.setMaxVelocity(v);
-	}
-	
-	public static boolean enemyIsRammer() {
-		return enemyApproachVelocity > 4.5;
-	}
+    public void setTurnRightRadians(double turn) {
+        super.setTurnRightRadians(turn);
+        robotPredictor.setTurnRightRadians(turn);
+    }
+
+    public void setAhead(double d) {
+        super.setAhead(d);
+        robotPredictor.setAhead(d);
+    }
+
+    public void setMaxVelocity(double v) {
+        super.setMaxVelocity(v);
+        robotPredictor.setMaxVelocity(v);
+    }
+
+    public static boolean enemyIsRammer() {
+        return enemyApproachVelocity > 4.5;
+    }
 }
